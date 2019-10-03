314e75e4d1bad376d92e9987e2d2c17310fcd553
kapil jhajhria <jhajhria44@gmail.com>
2019-10-03 12:55:09 +0530

    Completed with floating action button
    Now need to adapt it as per Ex UI

--- lib/main.dart
+++ lib/main.dart
@@ -6,17 +6,48 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(
     MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('RandomPasswords'),
-        ),
-        body: Container(), // Should be PasswordGenerator()
-      ),
+      home: Password(),
     ),
   );
 }
 
-List words = Words.list; // you are given list of words
+class Password extends StatefulWidget {
+  @override
+  _PasswordState createState() => _PasswordState();
+}
+
+class _PasswordState extends State<Password> {
+  List words = Words.list;
+  String randomPassword = "";
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('RandomPasswords'),
+      ),
+      body: Center(
+        child: Text(
+          '$randomPassword',
+          style: TextStyle(fontSize: 30),
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          setState(() {
+            String z = "";
+            for (int i = 0; i < 3; i++) {
+              int y = Random().nextInt(Words.list.length);
+              z = z + ' ' + Words.list[y];
+            }
+            randomPassword = z;
+          });
+        },
+        child: Icon(Icons.refresh),
+      ), // Should be PasswordGenerator()
+    );
+  }
+}
+
+// you are given list of words
 
 // Create a stateful widget called `PasswordGenerator` here
-
