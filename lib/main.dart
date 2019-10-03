30c768b9359bb6d4fd2e98167623f7856290328b
kapil jhajhria <jhajhria44@gmail.com>
2019-10-03 13:05:28 +0530

    Completed

--- lib/main.dart
+++ lib/main.dart
@@ -26,24 +26,51 @@ class _PasswordState extends State<Password> {
         title: Text('RandomPasswords'),
       ),
       body: Center(
-        child: Text(
-          '$randomPassword',
-          style: TextStyle(fontSize: 30),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              '$randomPassword',
+              style: TextStyle(fontSize: 30),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(14.0),
+              child: Card(
+                elevation: 2,
+                color: Colors.blue,
+                child: FlatButton(
+//                color: Colors.blue,
+                  child: Text('New Password',
+                      style: TextStyle(fontSize: 25, color: Colors.white)),
+                  onPressed: () {
+                    setState(() {
+                      String z = "";
+                      for (int i = 0; i < 3; i++) {
+                        int y = Random().nextInt(Words.list.length);
+                        z = z + ' ' + Words.list[y];
+                      }
+                      randomPassword = z;
+                    });
+                  },
+                ),
+              ),
+            ),
+          ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          setState(() {
-            String z = "";
-            for (int i = 0; i < 3; i++) {
-              int y = Random().nextInt(Words.list.length);
-              z = z + ' ' + Words.list[y];
-            }
-            randomPassword = z;
-          });
-        },
-        child: Icon(Icons.refresh),
-      ), // Should be PasswordGenerator()
+//      floatingActionButton: FloatingActionButton(
+//        onPressed: () {
+//          setState(() {
+//            String z = "";
+//            for (int i = 0; i < 3; i++) {
+//              int y = Random().nextInt(Words.list.length);
+//              z = z + ' ' + Words.list[y];
+//            }
+//            randomPassword = z;
+//          });
+//        },
+//        child: Icon(Icons.refresh),
+//      ), // Should be PasswordGenerator()
     );
   }
 }
